d422abbb63064190b9b1c1fa1f84995652ad7885
Ashish <ashish763shukla@gmail.com>
2024-02-11 19:45:43 +0530

    wordle2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { useState,useEffect } from "react";
+
 
 function App() {
+  const [solution,setSolution] = useState(null);
+
+  useEffect(()=>{
+    fetch('http://localhost:3001/solutions')
+      .then(res=> res.json())
+      .then(json=>{
+        const randomSolution = json[Math.floor(Math.random()*json.length)]
+        setSolution(randomSolution.word)
+      })
+  },[setSolution])
   return (
     <div className="App">
-      <h3>KSj,bskjbvdjj</h3>
+      <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
 
-export default App;
+export default App
